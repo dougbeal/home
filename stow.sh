562654690f81165b171b5377cc0ede2eec0e7e9b
Douglas Beal <git@dougbeal.com>
2012-04-19 00:35:20 -0700

    emacs on windows doesn't like symlinks, create a .emacs file that loads init.el

--- stow.sh
+++ stow.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+this=$( cd $(dirname ${BASH_SOURCE[0]}); pwd -P )
 perl=$(which perl)
 stow=$(which stow)
+  
+if [ ! -e ~/.emacs ]; then
+    echo "(load \"$this/emacs/init.el\")" > $HOME/.emacs
+fi
+
 $perl $stow --dir=. --target=$HOME home
 $perl $stow --dir=. --target=$HOME/.ssh .ssh
+
