9816e1968a79953884757eca3488f9c3c3e8a279
Douglas Beal <git@dougbeal.com>
2014-01-02 12:21:39 -0800

    comment out shell hack, should be superseeded by /etc/paths

--- emacs/dpb-osx.el
+++ emacs/dpb-osx.el
@@ -1,7 +1,8 @@
+;; shouldn't be necessary with /etc/paths setup properly
 ;; read in PATH from .bashrc
-(if (not (getenv "TERM_PROGRAM"))
-    (setenv "PATH"
-            (shell-command-to-string "source $HOME/.profile && printf $PATH")))
-(if (not (getenv "TERM_PROGRAM"))
-    (setenv "PYTHONPATH"
-            (shell-command-to-string "source $HOME/.profile && printf $PYTHONPATH")))
+;; (if (not (getenv "TERM_PROGRAM"))
+;;     (setenv "PATH"
+;;             (shell-command-to-string "source $HOME/.profile && printf $PATH")))
+;; (if (not (getenv "TERM_PROGRAM"))
+;;     (setenv "PYTHONPATH"
+;;             (shell-command-to-string "source $HOME/.profile && printf $PYTHONPATH")))
