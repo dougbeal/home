e775f8a182f5378957a7547436b71f6542db8e8b
Douglas Beal <git@dougbeal.com>
2015-03-30 14:06:51 -0700

    disable twittering

--- emacs/dpb-twittering.el
+++ emacs/dpb-twittering.el
@@ -1,10 +1,10 @@
 ;;https://github.com/dougbeal/twittering-mode
-(setq twittering-icon-mode t)
-(setq twittering-use-icon-storage t)
-(setq twittering-number-of-tweets-on-retrieval 200)
-(setq twittering-timer-interval 120)
-(setq twittering-display-remaining t)
-(twittering-enable-unread-status-notifier)
+;; (setq twittering-icon-mode t)
+;; (setq twittering-use-icon-storage t)
+;; (setq twittering-number-of-tweets-on-retrieval 200)
+;; (setq twittering-timer-interval 120)
+;; (setq twittering-display-remaining t)
+;; (twittering-enable-unread-status-notifier)
 
 ;; (setq twittering-initial-timeline-spec-string
 ;;       '("(:home+@)"
