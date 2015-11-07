d66b0850bf4ffbf1d993a454acfeec4f1c3cee14
Douglas Beal <git@dougbeal.com>
2015-11-07 10:43:26 -0800

    package not called iterm2

--- platform/osx/bootstrap.sh
+++ platform/osx/bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#bash "$(curl -fsSL https://raw.githubusercontent.com/dougbeal/home/master/platform/osx/bootstrap.sh)"
+#bash -xc "$(curl -fsSL https://raw.githubusercontent.com/dougbeal/home/master/platform/osx/bootstrap.sh)"
 git clone https://github.com/dougbeal/home ~/git/home
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install iterm2
 brew install emacs
-brew install Caskroom/cask/iterm2
+brew install Caskroom/cask/iterm
 brew install Caskroom/cask/divvy
 brew install Caskroom/cask/google-chrome
 brew install Caskroom/cask/google-drive
