905c9d1ebeebc04558c69078f2d1a3b61ad57207
Douglas Beal <git@dougbeal.com>
2015-11-07 11:03:30 -0800

    only ruby curl brew if necessary

--- platform/osx/bootstrap.sh
+++ platform/osx/bootstrap.sh
@@ -1,11 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 #bash -xc "$(curl -fsSL https://raw.githubusercontent.com/dougbeal/home/master/platform/osx/bootstrap.sh)"
 git clone https://github.com/dougbeal/home ~/git/home
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install iterm2
+command -v brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install stow
+brew install bash
+bash ~/git/home/install.sh
+brew install Caskroom/cask/brew-cask
 brew install emacs
-brew install Caskroom/cask/iterm
-brew install Caskroom/cask/divvy
-brew install Caskroom/cask/google-chrome
-brew install Caskroom/cask/google-drive
+brew cask install iterm
+brew cask install divvy
+brew cask install google-chrome
+brew cask install google-drive
+brew install coreutils
+brew install findutils
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
 
