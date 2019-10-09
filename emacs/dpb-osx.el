9885b0960e4ad82f1c5bb11bf1fd4c317e6072c0
Douglas Beal <public.git@dougbeal.com>
2019-10-09 11:58:02 -0700

    handle spaces in $HOME

--- emacs/dpb-osx.el
+++ emacs/dpb-osx.el
@@ -3,7 +3,7 @@
 ;; read in PATH from .bashrc
 (if (not (getenv "TERM_PROGRAM"))
     (setenv "PATH"
-            (shell-command-to-string "source $HOME/.profile && printf $PATH")))
+            (shell-command-to-string "source \"$HOME/.profile\" && printf \"$PATH\"")))
 ;; (if (not (getenv "TERM_PROGRAM"))
 ;;     (setenv "PYTHONPATH"
 ;;             (shell-command-to-string "source $HOME/.profile && printf $PYTHONPATH")))
