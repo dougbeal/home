5965b57ed291a4547c9c1b44ceaf7ebb23d368d4
Douglas Beal <git.public@dougbeal.com>
2020-02-23 13:36:03 -0800

    fj add k8s

--- package_install.sh
+++ package_install.sh
@@ -17,11 +17,16 @@ brew install gawk markdown git bash-completion cask ispell exiftool
         echo "ruby packages installed."
     )&
     ## os x development    
-    brew install go carthage fswatch
+    #brew install carthage 
+    brew install go faswatch
     brew cask install github-desktop
     (
         go get github.com/fatih/gomodifytags golang.org/x/tools/cmd/goimports
     )&
+    # kubernetes and kubeless
+    brew install kubernetes-cli kubeless helm
+    helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+    # kubectl create -f https://github.com/kubeless/kafka-trigger/releases/download/v1.0.2/kafka-zookeeper-v1.0.2.yaml
 )&
 
 
