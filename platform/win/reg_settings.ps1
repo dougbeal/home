5c96b89b3adaabc554c103621db915eae02200a8
Douglas Beal <public.git@dougbeal.com>
2020-05-12 11:57:49 -0700

    windows settings that require registry hacking

---
+++ platform/win/reg_settings.ps1
@@ -0,0 +1,13 @@
+#[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System] "DisableLockWorkstation"=dword:00000001
+
+# disable Windows-L, interferres with macOS/Barrier
+$lkWorkstation = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System'
+
+New-ItemProperty -Path $lkWorkstation -Value 1
+
+# 
+$hexified = "00,00,00,00,00,00,00,00,02,00,00,00,1d,00,3a,00,00,00,00,00".Split(',') | % { "0x$_"};
+
+$kbLayout = 'HKLM:\System\CurrentControlSet\Control\Keyboard Layout';
+
+New-ItemProperty -Path $kbLayout -Name "Scancode Map" -PropertyType Binary -Value ([byte[]]$hexified);
