333c003b583f8e59cbc1e31bed1c779f206990bd
Douglas Beal <public.git@dougbeal.com>
2020-05-12 12:10:40 -0700

    create path if it doesn't exist, supply name

--- platform/win/reg_settings.ps1
+++ platform/win/reg_settings.ps1
@@ -3,7 +3,12 @@
 # disable Windows-L, interferres with macOS/Barrier
 $lkWorkstation = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System'
 
-New-ItemProperty -Path $lkWorkstation -Value 1
+
+if (-not (Test-Path -Path $lkWorkstation))
+{
+    New-Item -Path $lkWorkstation 
+}
+New-ItemProperty -Path $lkWorkstation -Name DisableLockWorkstation -Value 1
 
 # 
 $hexified = "00,00,00,00,00,00,00,00,02,00,00,00,1d,00,3a,00,00,00,00,00".Split(',') | % { "0x$_"};
